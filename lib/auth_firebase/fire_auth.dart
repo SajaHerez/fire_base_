f6fa387da0fd7c196ade145bd74d57c6c6afe1b2
SajaHerez <herezsaja2020@gmail.com>
2023-02-21 12:19:59 +0200

    committed

---
+++ lib/auth_firebase/fire_auth.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class FireAuth {
+  static Future<User?> registerUsingEmailPassword({
+    required String name,
+    required String email,
+    required String password,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential creUser = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      user = creUser.user;
+      user?.updateDisplayName(name);
+      user?.reload();
+      user = auth.currentUser;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+      print(e.message);
+    }
+
+    return user;
+  }
+
+  static Future<User?> loginUsingEmailPassword({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+    try {
+      UserCredential creUser = await auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      user = creUser.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided.');
+      }
+    }
+    return user;
+  }
+
+  static Future<User?> refreshUser(User user) async {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  await user.reload();
+  User? refreshedUser = auth.currentUser;
+
+  return refreshedUser;
+}
+}
